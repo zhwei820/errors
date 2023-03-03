19fe056d52781e589e143015a9cdde250de4ba5b
周伟 <wei.zhou@matrixport.com>
2023-03-03 22:25:41 +0800

    u

--- example/grpc_error/main.go
+++ example/grpc_error/main.go
@@ -3,10 +3,11 @@ package main
 import (
 	"context"
 	"fmt"
-	"github.com/zhwei820/errors"
-	"github.com/zhwei820/errors/grpc_demo"
-	"google.golang.org/grpc"
 	"net"
+
+	"gitee.com/spwx/errors"
+	"gitee.com/spwx/errors/grpc_demo"
+	"google.golang.org/grpc"
 )
 
 var bizErrorCode uint32 = 10086
@@ -23,7 +24,8 @@ type Demo struct {
 
 func (d Demo) DoDemo(ctx context.Context, req *grpc_demo.Req) (*grpc_demo.Resp, error) {
 	// 将error封装成codeError后返回；
-	return nil, errors.ToGRPCReturnError(errors.NewBizCodeErrorf(bizErrorCode, "this is biz code error example"))
+	// return nil, errors.ToGRPCReturnError(errors.NewBizCodeErrorf(bizErrorCode, "this is biz code error example"))
+	return nil, errors.ToGRPCReturnError(errors.Annotate(errors.AlreadyExistsf(""), "AlreadyExistsf"))
 }
 
 // 运行一个grpc服务器
@@ -51,9 +53,11 @@ func grpcClient() {
 	cli := grpc_demo.NewDemoClient(conn)
 	_, err = cli.DoDemo(ctx, &grpc_demo.Req{})
 	codeErr := errors.GRPCErrToError(err)
-	if errors.IsBizCodeError(codeErr, bizErrorCode) {
-		fmt.Println("get the correct biz error code")
-	} else {
-		panic("get the wrong biz code")
-	}
+
+	fmt.Println("errors.IsAlreadyExists(err) ", errors.IsAlreadyExists(codeErr))
+	// if errors.IsBizCodeError(codeErr, bizErrorCode) {
+	// 	fmt.Println("get the correct biz error code")
+	// } else {
+	// 	panic("get the wrong biz code")
+	// }
 }
--- go.mod
+++ go.mod
@@ -1,14 +1,12 @@
-module github.com/zhwei820/errors
+module gitee.com/spwx/errors
 
 go 1.15
 
 require (
 	github.com/gin-gonic/gin v1.7.4
 	github.com/go-sql-driver/mysql v1.6.0
-	github.com/jinzhu/gorm v1.9.16
 	github.com/kr/pretty v0.3.0 // indirect
-	github.com/stretchr/testify v1.7.0
-	github.com/zhwei820/gconv v0.0.0-20220317121157-3c0d4e7dc487 // indirect
+	golang.org/x/sys v0.1.0 // indirect
 	golang.org/x/text v0.3.7
 	google.golang.org/grpc v1.41.0
 	google.golang.org/protobuf v1.27.1
